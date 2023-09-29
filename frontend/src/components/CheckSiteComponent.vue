abcdcaf5b6a403bc881eeb781eba3445fc34916a
Travis Briggs <audiodude@gmail.com>
2023-09-29 10:22:23 -0700

    Add TailwindCSS and TailwindCSS forms, re-work classes on main page to style them

--- frontend/src/components/CheckSiteComponent.vue
+++ frontend/src/components/CheckSiteComponent.vue
@@ -8,6 +8,9 @@ export default {
   },
   methods: {
     onSubmit() {
+      if (!this.$refs.form.checkValidity()) {
+        return;
+      }
       const url = `/api/v1/hello`;
       fetch(url)
         .then((res) => {
@@ -24,33 +27,38 @@ export default {
 </script>
 
 <template>
-  <form @submit.prevent="onSubmit()">
-    <label for="url">Gemini URL</label>
-    <div class="input-cont">
-      <input
-        v-model="url"
-        name="url"
-        type="url"
-        required
-        pattern="gemini://.*"
-        placeholder="gemini://my-cool-site.party"
-      />
+  <div class="w-full max-w-s">
+    <div class="mt-4 mb-4 text-center text-xl">
+      <div v-if="result">Yes, that is a Gemini site</div>
+      <div v-else-if="result === null">
+        Enter a gemini:// URL to check if it's serving a Gemini site
+      </div>
+      <div class="italic text-pink-500" v-else="!result">
+        Sorry, that Gemini site is down, or isn't a Gemini site at all
+      </div>
     </div>
-    <button type="submit">Check site</button>
-  </form>
-  <div v-if="result">Yes, that is a Gemini site</div>
-  <div v-else-if="result === null">
-    Enter a gemini:// URL to check if it's serving a Gemini site
+    <form ref="form" @submit.prevent="onSubmit()" class="container mx-auto" novalidate>
+      <div class="md:flex md:justify-center w-4/5 md:w-2/3 mx-auto">
+        <div class="grow my-4 md:my-0 mr-8">
+          <input
+            v-model="url"
+            name="url"
+            type="url"
+            required
+            pattern="gemini://.*"
+            placeholder="gemini://my-cool-site.party"
+            class="w-full px-2 py-1 border-solid border-black invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 rounded"
+          />
+        </div>
+        <div>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-1 rounded"
+            type="submit"
+          >
+            Check site
+          </button>
+        </div>
+      </div>
+    </form>
   </div>
-  <div v-else="!result">Sorry, that Gemini site is down, or isn't a Gemini site at all</div>
 </template>
-
-<style scoped>
-input:not(:placeholder-shown):invalid {
-  border: 2px dashed red;
-}
-
-input:valid {
-  border: 2px solid black;
-}
-</style>
