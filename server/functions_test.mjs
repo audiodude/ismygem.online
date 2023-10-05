afb8ca36d2b790fafffd00a12c78007cb3f5cc0a
Travis Briggs <audiodude@gmail.com>
2023-10-04 18:55:25 -0700

    Updated functions_test.mjs

--- server/functions_test.mjs
+++ server/functions_test.mjs
@@ -3,8 +3,13 @@ import { postCheckSite } from './functions.mjs';
 describe('functions test', () => {
   describe('postCheckSite', () => {
     let res;
+    let checkSiteSpy;
+    let gemRequestSpy;
+
     beforeEach(() => {
       res = jasmine.createSpyObj('res', ['json']);
+      checkSiteSpy = jasmine.createSpy('checkSite');
+      gemRequestSpy = jasmine.createSpy('gemRequest');
     });
 
     describe(`when the request body doesn't have a url property`, () => {
@@ -16,6 +21,37 @@ describe('functions test', () => {
           message: 'POST request was not JSON or missing `url` field',
         });
       });
+    });
+
+    it('passes the right parameters to checkSite', () => {
+      postCheckSite(
+        { body: { url: 'gemini://foo.bar.fake' } },
+        res,
+        checkSiteSpy,
+        gemRequestSpy,
+      );
+      expect(checkSiteSpy).toHaveBeenCalledWith(
+        'gemini://foo.bar.fake',
+        gemRequestSpy,
+        jasmine.any(Function),
+      );
+    });
+
+    it('returns the result from checkSite to res.json', () => {
+      let resultCallback;
+      checkSiteSpy.and.callFake((url, gemRequest, callback) => {
+        resultCallback = callback;
+      });
+
+      postCheckSite(
+        { body: { url: 'gemini://foo.bar.fake' } },
+        res,
+        checkSiteSpy,
+        gemRequestSpy,
+      );
+
+      resultCallback({ result: true, message: null });
+      expect(res.json).toHaveBeenCalledWith({ result: true, message: null });
     });
   });
 });
